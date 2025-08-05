4e87d84a86f432cd954196259debf0af15d1d879
Calebe Medeiros <franciscocalebe@gmail.com>
2025-08-05 11:06:33 -0300

    feat: add sign up form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <div className="dark">
-    <Button>Boorcamp!!</Button>
-  </div>
+  return (
+    <div>
+      <Button>Boorcamp!!</Button>
+    </div>
+  );
 }
